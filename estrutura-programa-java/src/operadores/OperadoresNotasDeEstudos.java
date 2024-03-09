b732c610cf37a00084a72477f882701a924b0e44
Elievelton <elievelton@gmail.com>
2024-03-09 00:31:01 -0300

    novos operadores

---
+++ estrutura-programa-java/src/operadores/OperadoresNotasDeEstudos.java
@@ -0,0 +1,43 @@
+package operadores;
+
+public class OperadoresNotasDeEstudos {
+    public static void main(String[] args) {
+
+        //Operação de soma
+        int numero1 = 1;
+        int numero2 = 2;
+        int soma = numero1 + numero2;
+        System.out.println("\n A soma dos numeros é : " + soma);
+
+        //Operação de subtração
+        int numero3 = 3;
+        int numero4 = 4;
+        int subtracao = numero4 - numero3;
+        System.out.println("\n A subtracao dos numeros é : " + subtracao);
+
+        //Operação de multiplicação
+        int numero5 = 5;
+        int numero6 = 6;
+        int multiplicacao = numero5 * numero6;
+        System.out.println("\n A multiplicacao dos numeros é : " + multiplicacao);
+
+        //Operação de Divisão
+        int numero7 = 7;
+        int numero8 = 70;
+        int divisao = numero8 / numero7;
+        System.out.println("\n A divisão dos numeros é : " + divisao);
+
+        //Operação de Modulo
+        int numero9 = 9;
+        int numero10 = 91;
+        int modulo = numero10 % numero9;
+        System.out.println("\n O Modulo do numero10 e numero 9 é:"+ modulo);
+
+        //Operação de decremento e incremento
+        int contador = 0;
+        contador++;  
+        contador++;
+        System.err.println("\n O incremento é de "+ contador);
+
+    }
+}
